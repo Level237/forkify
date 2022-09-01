0e0398fd9cf6375f5b5aaa43a6b8cedbc45d6709
level237 <Bramslevel129@gmail.com>
2022-09-01 15:26:03 -0400

    Building the Search View Part 2

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -6,6 +6,21 @@ export const clearInput=()=>{elements.searchInput.value='';}
 export const clearResults=()=>{
     elements.searchResList.innerHTML='';
 }
+
+const limiRecipeTitle=(title,limit=17)=>{
+    const newTitle=[];
+    if(title.length > limit){
+        title.split(' ').reduce((acc,curr)=>{
+            if(acc + curr.length <=limit){
+                newTitle.push(curr);
+            }
+            return acc + curr.length;
+        },0);
+
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
 const renderRecipe=recipe=>{
     const markup=`
     <li>
@@ -14,7 +29,7 @@ const renderRecipe=recipe=>{
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limiRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
