4c1f270a37daa23615dd58dce2641b026fa0d81b
level237 <Bramslevel129@gmail.com>
2022-09-08 13:10:41 -0400

    updating Recipe Servings

--- src/js/index.js
+++ src/js/index.js
@@ -105,4 +105,21 @@ const controlRecipe= async()=>{
 //window.addEventListener('hashchange',controlRecipe);
 //window.addEventListener('load',controlRecipe);
 
-['hashchange','load'].forEach(event => window.addEventListener(event,controlRecipe))
+['hashchange','load'].forEach(event => window.addEventListener(event,controlRecipe));
+
+//Handling recipe button clicks;
+elements.recipe.addEventListener('click',e=>{
+    if(e.target.matches('.btn-decrease,.btn-decrease *')){
+        // Decrease button is clicked
+        if(state.recipe.servings > 1){
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe)
+        }
+        
+    }else if(e.target.matches('.btn-increase,.btn-increase *')){
+        //increase button is clicked
+        state.recipe.updateServings('inc')
+        recipeView.updateServingsIngredients(state.recipe)
+    }
+    console.log(state.recipe);
+});
